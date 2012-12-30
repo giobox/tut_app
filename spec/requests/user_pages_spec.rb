58299c1697ea43850220b5275c7b0ec97c81bd44
Giovanni Coia <giovanni@thecoia.com>
2012-12-30 11:57:06 +0000

    refactoring forms to partials

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,10 +41,10 @@ describe "signup" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "user_name",         with: "Example User"
+        fill_in "user_email",        with: "user@example.com"
+        fill_in "user_password",     with: "foobar"
+        fill_in "user_password_confirmation", with: "foobar"
       end
       describe "after saving the user" do
         before { click_button submit }
